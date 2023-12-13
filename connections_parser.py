b3dd6c5efb91e00cd55ee7143adedd82c4bcb908
Gabriel DiFiore <difioregabe@gmail.com>
2023-12-12 19:05:15 -0500

    Update connections_parser.py

--- connections_parser.py
+++ connections_parser.py
@@ -1,10 +1,34 @@
 import re
 
 def validate_input(user_input):
+    """
+    Validates the format and content of the user input for a Connections puzzle.
+
+    Args:
+        user_input (str): The user-provided text input for the puzzle.
+
+    Returns:
+        bool: True if the input is valid, False otherwise.
+
+    Prints error messages for specific validation failures.
+
+    Validation Criteria:
+    - The input must have at least 6 lines.
+    - The first line must start with 'Connections'.
+    - The second line must start with 'Puzzle #'.
+    - Extracted puzzle number must be a valid integer.
+    - Emojis must be in the set {'🟩', '🟨', '🟪', '🟦'}.
+    - There should be no more than 7 rows of emojis.
+    - Each row must contain no more than 4 emojis.
+
+    If any of these criteria are not met, corresponding error messages are printed,
+    and the function returns False, indicating invalid input.
+    """
+
     lines = user_input.splitlines()
 
     # check if the input has the expected structure
-    if len(lines) < 3 or not lines[0].startswith('Connections') or not lines[1].startswith('Puzzle #'):
+    if len(lines) < 6 or not lines[0].startswith('Connections') or not lines[1].startswith('Puzzle #'):
         print("Invalid input format")
         return False
 
@@ -30,6 +54,21 @@ def validate_input(user_input):
                 return False
 
 def parse_connections_puzzle(puzzle_text):
+    """
+    Parses a Connections puzzle from the provided puzzle_text.
+
+    Args:
+        puzzle_text (str): The text representation of the Connections puzzle.
+
+    Returns:
+        tuple: A tuple containing the puzzle number and results.
+               - If the input is invalid, returns (None, None).
+               - Otherwise, returns (puzzle_number, results) where:
+                 - puzzle_number (int): The puzzle number extracted from the input.
+                 - results (dict): A dictionary mapping puzzle categories to correctness.
+                   - Categories include 'Yellow' (Easiest), 'Green', 'Blue', and 'Purple' (Hardest).
+                   - Each category is associated with a boolean indicating correctness.
+    """
     if (validate_input(puzzle_text) == False):
         return None, None # invalid input
 
