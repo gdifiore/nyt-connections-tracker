338e2640e30bac8195d759217eea4f0a6392cc89
Gabriel DiFiore <difioregabe@gmail.com>
2023-12-20 15:22:22 -0500

    Revert "add avg guesses calculation"
    This reverts commit d0b50dcf45753c0521b7209746b621e78a9a7b45.

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,3 @@
-function updateLocalStorage(guessed, guesses) {
-    calculatePercentages();
-    calculateAverageGuesses(guessed, guesses);
-}
-
 function calculatePercentages() {
     console.log("calculating percentages");
 
@@ -29,18 +24,4 @@ function calculatePercentages() {
 
     document.getElementById('greenProgressBar').style.width = greenPercentage + '%';
     document.getElementById('greenProgressBar').innerText = greenPercentage + '%';
-}
-
-function calculateAverageGuesses(guessed, guesses) {
-    if (guessed)
-    {
-        const avg = parseInt(localStorage.getItem('averageGuesses')) || 0;
-        const tot = parseInt(localStorage.getItem('totalPuzzles')) || 0;
-
-        const newAvg = ((avg * tot) + guesses) / (tot + 1);
-
-        localStorage.setItem('averageGuesses', newAvg);
-
-        document.getElementById('averageGuesses').innerText = newAvg.toFixed(2);
-    }
 }
